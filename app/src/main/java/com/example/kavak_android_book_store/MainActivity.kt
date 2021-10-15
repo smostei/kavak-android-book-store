7caf536943dcbbc90e8bdd6960a86e27079c17f4
Santiago <santiago.mosteiro@uala.com.ar>
2021-10-15 00:04:57 -0300

    init android project

---
+++ app/src/main/java/com/example/kavak_android_book_store/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kavak_android_book_store
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
